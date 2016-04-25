ef570288ddb90fdcaece66b3bc9a149ba5c277ea
trentbrown <trent@thebodyofchrist.us>
2016-04-25 16:30:49 -0500

    Speaker Feed

--- scripts/getSpeakers.js
+++ scripts/getSpeakers.js
@@ -55,50 +55,12 @@ function loadSpeakers(search, communityID, insert) {
     jQuery.ajax({
         url: url,
         type: "GET",
-    }).done(function(speakerData, textStatus, jqXHR) {
-        console.log("HTTP Request Succeeded: " + jqXHR.status);
-        window.speakerData = speakerData;
-        if (url.split(':')[0] == "https") {
-
-        } else {
-        downloadFile(name, url);
-        }
+    }).done(function(speakerData, textStatus, jqXHR) {    
         if (insert == "True") {
-        $('#contentHolder').append('<div class="demo-charts mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid" style="height: 52px;width:100% !important;"><span style="margin-top: 8px;font-size: 17px;">' + speakerData.count +   ' Speakers/Teachers Found ' + ' </span></div>');
+    $('#contentHolder').append(speakerData);
         }else{}
-        $.each(speakerData.results, function(index, value) {
-
-            if (speakerData.results[index].speakerimage == null) {
-                speakerImage = 'https://www.thebodyofchrist.us/static/images/default-avatar.png'
-            } else {
-                speakerImage = speakerData.results[index].speakerimage;
-            }
-            if (insert == "True") {
-                $(target).append('<div class="demo-charts mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid" style="display:block;">' +
-                    '<ul class="demo-list-two mdl-list"> <center>' +
-                    '<li class="mdl-list__item mdl-list__item--two-line" style="height:90px;">' +
-                    '<span class="mdl-list__item-primary-content" style="margin-top:-15px;width:100%;">' +
-                    //'<div style="background-size:cover;border-radius:20px;margin-top:-15px;width:80px;height:80px;float:left;margin-right:20px;background-image:url(\'' + speakerImage + '\');"></div>' +
-                    '<img style="border-radius:20px;margin-top:-15px;width:80px;height:80px;position:absolute;" id="speaker_IMG_' + speakerData.results[index].speakerid + '" src="' + speakerImage + '" width="80px" height="80px" style="float:left;margin-right:20px;"/>' +
-                    '<span style="margin-left:100px;">' + speakerData.results[index].speakername + '</span>' +
-                    '<span class="mdl-list__item-sub-title" style="margin-left:100px;">' + speakerData.results[index].sermonCount + ' Sermons / Teachings</span>' +
-                    '</span>' +
-                    '</li>' +
-                    '</ul>' +
-                    '</div>');
-                 var app = document.URL.indexOf('http://') === -1 && document.URL.indexOf('https://') === -1;
-            if (app) {
-                var imageCacheTarget = $('#speaker_IMG_' + speakerData.results[index].speakerid);
-                cacheImageCheck(imageCacheTarget);
-            } else {
-
-            }
-            } else {}
-        });
-
-        $('#loading').hide();
+      
     });
-}
 
 function cacheImageCheck(imageCacheTarget) {
 
