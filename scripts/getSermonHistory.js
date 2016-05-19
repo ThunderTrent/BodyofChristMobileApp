2cf07a8ec35b1c74359c26a20c883a6f6b96b203
trentbrown <trent@thebodyofchrist.us>
2016-05-18 22:24:15 -0500

    serach bar update

--- scripts/getSermonHistory.js
+++ scripts/getSermonHistory.js
@@ -28,28 +28,21 @@ function sermonHistoryInsert(url, target, name) {
         type: "GET",
     }).done(function(sermonHistoryData, textStatus, jqXHR) {
 
-      $('#filtersSermons').after('<div class="demo-charts mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid" style="width:100% !important;height: 52px;background-color: #DE5F4D !important;margin-left:0px;"><input id="searchHistoryInput" style="width:90%;height:34px;margin-left:5%;outline:none !important;border:none !important;background-color:#EC8374;font-weight: 500;font-size: 14px;color:white;padding-left:20px;text-transform: uppercase;font-family:"Open Sans", sans-serif;"></div>')
+      $('#filtersSermons').after('<div class="demo-charts mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid" style="width:100% !important;height: 52px;background-color: #DE5F4D !important;margin-left:0px;margin-top:25px;"><input id="searchHistoryInput" style="width:90%;height:34px;margin-left:5%;outline:none !important;border:none !important;background-color:#EC8374;font-weight: 500;font-size: 14px;color:white;padding-left:20px;text-transform: uppercase;font-family:"Open Sans", sans-serif;"></div>')
 
       $('#contentHolder').append(sermonHistoryData);
 
 $( "#loader-wrapper" ).fadeOut( "slow", function() {
 
-$( "#sermonHistoryInput" ).change(function() {
-
+$("searchHistoryInput").on("change paste keyup", function() {
  $('#contentHolder').empty();
-
  query = document.getElementById('searchHistoryInput').value;
-
  jQuery.ajax({
         url: 'https://www.thebodyofchrist.us/service/phonegap/sermonhistory/?q=' + query,
         type: "GET",
     }).done(function(sermonHistoryData, textStatus, jqXHR) {
     $('#contentHolder').append(sermonHistoryData);
     });
-
-
-
-
 });
 
     });
